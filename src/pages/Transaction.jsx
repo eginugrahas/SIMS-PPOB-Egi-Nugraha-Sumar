7678f19a63ae2bf5270c96bcec3eb9f8f92abb19
eginugrahas <eginugrahasmart@gmail.com>
2023-10-05 21:55:02 +0700

    check loop

--- src/pages/Transaction.jsx
+++ src/pages/Transaction.jsx
@@ -7,6 +7,7 @@ import { useDispatch } from "react-redux";
 function TransactionPage() {
   const token = useSelector((state) => state.auth.token);
   const transaction = useSelector((state) => state.transaction.transaction);
+  console.log(transaction);
   const [offset, setOffset] = useState(0);
   const dispatch = useDispatch();
   function handleShowMore() {
@@ -21,39 +22,35 @@ function TransactionPage() {
     <div className="">
       <div className="text-lg my-3">Semua Transaksi</div>
       <div className="flex flex-col gap-4">
-        {transaction ? (
-          transaction.map((item) => {
-            return (
-              <div
-                key={item.invoice_number}
-                className="border flex justify-between border-gray rounded p-3 w-full items-center"
-              >
-                <div className="">
-                  <div
-                    className={`${
-                      item.transaction_type === "TOPUP"
-                        ? "text-light-green"
-                        : "text-red"
-                    } font-medium `}
-                  >
-                    {item.transaction_type === "TOPUP" ? "+ " : "- "}
-                    Rp{parsingRibuan(item.total_amount)}
-                  </div>
-                  <div className="text-xs text-gray">
-                    {formatDateTime(item.created_on)}
-                  </div>
+        {transaction.map((item) => {
+          return (
+            <div
+              key={item.invoice_number}
+              className="border flex justify-between border-gray rounded p-3 w-full items-center"
+            >
+              <div className="">
+                <div
+                  className={`${
+                    item.transaction_type === "TOPUP"
+                      ? "text-light-green"
+                      : "text-red"
+                  } font-medium `}
+                >
+                  {item.transaction_type === "TOPUP" ? "+ " : "- "}
+                  Rp{parsingRibuan(item.total_amount)}
                 </div>
-                <div className="text-sm">{item.description}</div>
+                <div className="text-xs text-gray">
+                  {formatDateTime(item.created_on)}
+                </div>
               </div>
-            );
-          })
-        ) : (
-          <div className="text-center">Belum ada transaksi</div>
-        )}
+              <div className="text-sm">{item.description}</div>
+            </div>
+          );
+        })}
       </div>
       <div
         className={`text-center text-red text-sm mt-4 cursor-pointer ${
-          transaction.length < 5 && "hidden"
+          transaction ? (transaction.length < 5 ? "" : "hidden") : "hidden"
         }`}
         onClick={handleShowMore}
       >
